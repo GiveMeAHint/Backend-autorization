39baf1a82e3647d9647eeafa6635d3d84be3b5ca
Irina Kalmykova <ikalmyckova@yandex.ru>
2025-12-18 00:03:14 +0500

    second commit

--- auth_project/auth_project/urls.py
+++ auth_project/auth_project/urls.py
@@ -1,9 +1,26 @@
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
 from rest_framework_simplejwt.views import TokenRefreshView
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Auth API",
+        default_version='v1',
+        description="API для аутентификации",
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('accounts.urls')),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    
+    # Swagger
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
